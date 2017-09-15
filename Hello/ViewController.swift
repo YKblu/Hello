c291e53736ce34a0bab8f3995d9e7e2e5a9a4aed
Yongkuk Ju <yongkukju@Yongkukui-MacBook-Pro.local>
2017-09-15 14:32:02 +0900

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Yongkuk Ju on 2017. 9. 15..
+//  Copyright © 2017년 myname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
